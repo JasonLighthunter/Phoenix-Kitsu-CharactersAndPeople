e9626251a8456a05c8100b4e83ac44c33a8bea14
Job Cuppen <jobcuppen@live.nl>
2018-01-11 17:51:11 +0100

    Update AnimeProduction.swift

--- Sources/PhoenixKitsuCharactersAndPeople/AnimeProduction.swift
+++ Sources/PhoenixKitsuCharactersAndPeople/AnimeProduction.swift
@@ -9,10 +9,4 @@ public class AnimeProductionAttributes: KitsuObjectAttributes {
   public let createdAt: String?
   public let updatedAt: String?
   public let role: AnimeProductionRoleEnum
-  
-  private enum CodingKeys: String, CodingKey {
-    case createdAt
-    case updatedAt
-    case role
-  }
 }
